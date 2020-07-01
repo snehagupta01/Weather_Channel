32411e8497c81606f23f892e838a5b1274cc9072
snehagupta01 <sneha2001gupta@gmail.com>
2020-07-01 18:16:36 +0530

    getting actual weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '3c78ee89bff4691480c85ed31193fedf';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,18 +10,29 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
+  double latitude;
+  double longitude;
+  void getLocationData() async {
     Location location = new Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+//    print(location.latitude);
+//    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = new NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+
+    print(weatherData);
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
   @override
